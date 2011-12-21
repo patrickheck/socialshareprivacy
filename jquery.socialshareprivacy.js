3dc634db0260b7a0b314e9522ef5571ee3010655
patrickheck <patrick@patrickheck.de>
2011-12-21 12:44:10 +0100

    replaced all images with one sprite

--- jquery.socialshareprivacy.js
+++ jquery.socialshareprivacy.js
@@ -114,7 +114,7 @@
                 },
                 'gplus' : {
                     'status'            : 'on',
-                    'dummy_img'         : 'socialshareprivacy/images/dummy_gplus.png',
+                    'dummy_img'         : '',
                     'txt_info'          : '2 Klicks f&uuml;r mehr Datenschutz: Erst wenn Sie hier klicken, wird der Button aktiv und Sie k&ouml;nnen Ihre Empfehlung an Google+ senden. Schon beim Aktivieren werden Daten an Dritte &uuml;bertragen &ndash; siehe <em>i</em>.',
                     'txt_gplus_off'     : 'nicht mit Google+ verbunden',
                     'txt_gplus_on'      : 'mit Google+ verbunden',
@@ -173,23 +173,24 @@
             if (facebook_on) {
                 var fb_enc_uri = encodeURIComponent(uri + options.services.facebook.referrer_track);
                 var fb_code = '<iframe src="http://www.facebook.com/plugins/like.php?locale=' + options.services.facebook.language + '&amp;href=' + fb_enc_uri + '&amp;send=false&amp;layout=button_count&amp;width=120&amp;show_faces=false&amp;action=' + options.services.facebook.action + '&amp;colorscheme=light&amp;font&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:145px; height:21px;" allowTransparency="true"></iframe>';
+				var fb_dummy_btn;
 				if (options.services.facebook.dummy_img)
 				{
-					var fb_dummy_btn = '<div  class="fb_like_privacy_dummy"><img src="' + options.services.facebook.dummy_img + '" alt="Facebook &quot;Like&quot;-Dummy"/></div>';
+					fb_dummy_btn = '<img class="fb_like_privacy_dummy" src="' + options.services.facebook.dummy_img + '" alt="' + options.services.facebook.dummy_caption + '" />';
 				}
 				else
 				{
-					var fb_dummy_btn = '<div class="fb_like_privacy_dummy"><div class="start"></div><div class="center"><span class="label">' + options.services.facebook.dummy_caption + '</span></div><div class="end"></div></div>';
+					fb_dummy_btn = '<div class="fb_like_privacy_dummy"><span>' + options.services.facebook.dummy_caption + '</span></div>';
 				}
                 context.append('<li class="facebook help_info"><span class="info">' + options.services.facebook.txt_info + '</span><span class="switch off">' + options.services.facebook.txt_fb_off + '</span><div class="fb_like dummy_btn">' + fb_dummy_btn + '</div></li>');
 
                 var $container_fb = $('li.facebook', context);
 
-                $('li.facebook div.fb_like div.fb_like_privacy_dummy,li.facebook span.switch', context).live('click', function () {
+                $('li.facebook div.fb_like .fb_like_privacy_dummy,li.facebook span.switch', context).live('click', function () {
                     if ($container_fb.find('span.switch').hasClass('off')) {
                         $container_fb.addClass('info_off');
                         $container_fb.find('span.switch').addClass('on').removeClass('off').html(options.services.facebook.txt_fb_on);
-                        $container_fb.find('div.fb_like_privacy_dummy').replaceWith(fb_code);
+                        $container_fb.find('.fb_like_privacy_dummy').replaceWith(fb_code);
                     } else {
                         $container_fb.removeClass('info_off');
                         $container_fb.find('span.switch').addClass('off').removeClass('on').html(options.services.facebook.txt_fb_off);
@@ -212,24 +213,25 @@
                 var twitter_enc_uri = encodeURIComponent(uri + options.services.twitter.referrer_track);
                 var twitter_count_url = encodeURIComponent(uri);
                 var twitter_code = '<iframe allowtransparency="true" frameborder="0" scrolling="no" src="http://platform.twitter.com/widgets/tweet_button.html?url=' + twitter_enc_uri + '&amp;counturl=' + twitter_count_url + '&amp;text=' + text + '&amp;count=horizontal&amp;lang=' + options.services.twitter.language + '" style="width:130px; height:25px;"></iframe>';
+				var twitter_dummy_btn
 				if (options.services.twitter.dummy_img)
 				{
-					var twitter_dummy_btn = '<div  class="tweet_this_dummy"><img src="' + options.services.twitter.dummy_img + '" alt="&quot;Tweet this&quot;-Dummy"/></div>';
+					twitter_dummy_btn = '<img class="tweet_this_dummy" src="' + options.services.twitter.dummy_img + '" alt="' + options.services.twitter.dummy_caption + '" />';
 				}
 				else
 				{
-					var twitter_dummy_btn = '<div class="tweet_this_dummy"><div class="start"></div><div class="center"><span class="label">' + options.services.twitter.dummy_caption + '</span></div><div class="end"></div></div>';
+					twitter_dummy_btn = '<div class="tweet_this_dummy"><span>' + options.services.twitter.dummy_caption + '</span></div>';
 				}
 
                 context.append('<li class="twitter help_info"><span class="info">' + options.services.twitter.txt_info + '</span><span class="switch off">' + options.services.twitter.txt_twitter_off + '</span><div class="tweet dummy_btn">' + twitter_dummy_btn + '</div></li>');
 
                 var $container_tw = $('li.twitter', context);
 
-                $('li.twitter div.tweet_this_dummy,li.twitter span.switch', context).live('click', function () {
+                $('li.twitter .tweet_this_dummy,li.twitter span.switch', context).live('click', function () {
                     if ($container_tw.find('span.switch').hasClass('off')) {
                         $container_tw.addClass('info_off');
                         $container_tw.find('span.switch').addClass('on').removeClass('off').html(options.services.twitter.txt_twitter_on);
-                        $container_tw.find('div.tweet_this_dummy').replaceWith(twitter_code);
+                        $container_tw.find('.tweet_this_dummy').replaceWith(twitter_code);
                     } else {
                         $container_tw.removeClass('info_off');
                         $container_tw.find('span.switch').addClass('off').removeClass('on').html(options.services.twitter.txt_twitter_off);
@@ -247,17 +249,21 @@
                 
                 // we use the Google+ "asynchronous" code, standard code is flaky if inserted into dom after load
                 var gplus_code = '<div class="g-plusone" data-size="medium" data-href="' + gplus_uri + '"></div><script type="text/javascript">window.___gcfg = {lang: "' + options.services.gplus.language + '"}; (function() { var po = document.createElement("script"); po.type = "text/javascript"; po.async = true; po.src = "https://apis.google.com/js/plusone.js"; var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(po, s); })(); </script>';
-                var gplus_dummy_btn = '<img src="' + options.services.gplus.dummy_img + '" alt="&quot;Google+1&quot;-Dummy" class="gplus_one_dummy" />';
-
+                var gplus_dummy_btn
+				if (options.services.gplus.dummy_img) {
+					gplus_dummy_btn = '<img src="' + options.services.gplus.dummy_img + '" alt="+1" class="gplus_one_dummy" />';
+				} else {
+					gplus_dummy_btn = '<div class="gplus_one_dummy">+1</div>';
+				}
                 context.append('<li class="gplus help_info"><span class="info">' + options.services.gplus.txt_info + '</span><span class="switch off">' + options.services.gplus.txt_gplus_off + '</span><div class="gplusone dummy_btn">' + gplus_dummy_btn + '</div></li>');
 
                 var $container_gplus = $('li.gplus', context);
 
-                $('li.gplus div.gplusone img,li.gplus span.switch', context).live('click', function () {
+                $('li.gplus div.gplusone .gplus_one_dummy,li.gplus span.switch', context).live('click', function () {
                     if ($container_gplus.find('span.switch').hasClass('off')) {
                         $container_gplus.addClass('info_off');
                         $container_gplus.find('span.switch').addClass('on').removeClass('off').html(options.services.gplus.txt_gplus_on);
-                        $container_gplus.find('img.gplus_one_dummy').replaceWith(gplus_code);
+                        $container_gplus.find('.gplus_one_dummy').replaceWith(gplus_code);
                     } else {
                         $container_gplus.removeClass('info_off');
                         $container_gplus.find('span.switch').addClass('off').removeClass('on').html(options.services.gplus.txt_gplus_off);
