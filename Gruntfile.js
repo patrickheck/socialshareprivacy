22e6070fc863cc99629720cbf7de832e4dc37e16
Patrick Heck <patrick@patrickheck.de>
2013-10-31 16:28:03 +0100

    mist

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
-      all: ['Gruntfile.js', 'jquery.socialshareprivacy.js']
+      all: ['Gruntfile.js', 'jquery.socialshareprivacy.js', 'test/socialshareprivacy_test.js']
     },
     uglify: {
       options: {
